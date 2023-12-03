568b2e94bd91162ad3210f3a63c229c58cce3f2a
Tazrian Zaman Tushti <73343497+Tazrian1999@users.noreply.github.com>
2023-12-03 20:01:33 +0600

    commit files

---
+++ cart/cart.py
@@ -0,0 +1,28 @@
+class Cart():
+    def __init__(self, request):
+        self.session = request.session
+        
+        # Get session key
+        cart = self.session.get('session_key')
+
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
+        
+        # Make available
+        self.cart = cart 
+
+    def add(self, product):
+        product_id = str(product.id)
+        
+        # Login
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True
+
+    def __len__(self):
+        return len(self.cart)
+
+            
